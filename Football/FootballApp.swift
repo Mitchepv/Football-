ffe5dc19066b3239ba757e88aeb0886b850f939f
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-01-12 21:50:28 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Nia Mitchell on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
